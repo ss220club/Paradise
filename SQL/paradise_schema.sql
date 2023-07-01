1174c0aaf4909670077feba9bbcd7d4f8cd72707
Furior <furiorg@gmail.com>
2023-07-02 04:11:36 +0700

    Fix: typo

--- SQL/paradise_schema.sql
+++ SQL/paradise_schema.sql
@@ -175,7 +175,7 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB AUTO_INCREMENT=99 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-DROP TABLE IF EXISTS `admin`;
+DROP TABLE IF EXISTS `admin_wl`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `admin_wl` (
